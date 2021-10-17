9a096cb09c8f5d3c5c28be10f8e2377fc925c5c3
mcbethr <51751681+mcbethr@users.noreply.github.com>
2021-10-17 10:35:17 -0400

    Initial commit

---
+++ SouthWorstLuggageWrapper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SouthWorstLuggageWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SouthWorstDLLAdapter.DisplayEntry();
+        }
+    }
+}
---
+++ SouthWorstLuggageWrapper/SouthWorstDLLAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouthWorstLuggageWrapper
+{
+    public static class SouthWorstDLLAdapter
+    {
+        
+        // Import the libc and define the method to represent the native function.
+        //[DllImport("SouthWorstLuggageEstimationLibrary.dll")]
+        //private static extern int AddTwoNumbers(int one, int two);
+
+        [DllImport("SouthWorstLuggageEstimationLibrary.dll", EntryPoint = "MyPrint")]
+        static extern void MyPrint(string message);
+
+        // Implement the above DirClbk delegate;
+        // this one just prints out the filename that is passed to it.
+        public static int DisplayEntry()
+        {
+            MyPrint("Hello World C# => C++");
+            return 0;
+        }
+
+    }
+}
